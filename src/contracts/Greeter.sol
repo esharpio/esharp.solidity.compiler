26e2d29bd919a2e70df9f1b06f8b0a5eb0706f89
Lucas Cullen <bitcoinbrisbane@users.noreply.github.com>
2021-03-03 15:28:33 +1000

    Update Greeter.sol

--- src/contracts/Greeter.sol
+++ src/contracts/Greeter.sol
@@ -4,18 +4,7 @@ pragma solidity ^0.4.2;
 // Example taken from https://www.ethereum.org/greeter, also used in
 // https://github.com/ethereum/go-ethereum/wiki/Contract-Tutorial#your-first-citizen-the-greeter
 
-contract Mortal {
-    /* Define variable owner of the type address*/
-    address owner;
-
-    /* this function is executed at initialization and sets the owner of the contract */
-    constructor() public { owner = msg.sender; }
-
-    /* Function to recover the funds on the contract */
-    function kill() public { if (msg.sender == owner) selfdestruct(owner); }
-}
-
-contract Greeter is Mortal {
+contract Greeter {
     /* define variable greeting of the type string */
     string greeting;
 
