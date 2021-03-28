d0208a9652113f26f6f02a4381b7772d527684e7
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2021-03-28 21:02:57 +1000

    Chore: Update

--- src/esharp.solidity.compiler/Parser.cs
+++ src/esharp.solidity.compiler/Parser.cs
@@ -1,10 +1,16 @@
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using esharp.solidity.compiler.Syntax;
 
 namespace esharp.solidity.compiler
 {
     public class Parser
     {
+        //private readonly DiagnosticBag _diagnostics = new DiagnosticBag();
+        private readonly SyntaxTree _syntaxTree;
+        //private readonly SourceText _text;
+        private readonly ImmutableArray<SyntaxToken> _tokens;
         private int _position;
 
         public Parser(string text)
@@ -24,8 +30,8 @@ namespace esharp.solidity.compiler
                 }
             } while (token.Kind != SyntaxKind.EndOfFileToken);
 
-            _tokens = tokens.ToArray();
-            _diagnostics.AddRange(lexer.Diagnostics);
+            _tokens = tokens.ToImmutableArray();
+            //_diagnostics.AddRange(lexer.Diagnostics);
         }
     }
 }
